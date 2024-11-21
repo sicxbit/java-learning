256e5acf4fba8ad85036f28247426a6d755a7b2d
Arunjith <arunjith.unofficial@gmail.com>
2024-11-21 21:20:50 +0530

    added the practiced exercise to the scanner01 file

--- scanner/scanner01.java
+++ scanner/scanner01.java
@@ -1,1 +1,20 @@
-// to be done
+// writing a program to calculate the total number of fruits ( apples and oranges ) by using scanner
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // Use a Scanner to read user input
+        Scanner sc = new Scanner(System.in);
+
+        // Read the number of apples from the user
+        int apples = sc.nextInt();
+
+        // Read the number of oranges from the user
+        int oranges = sc.nextInt();
+
+        // Calculate the total number of fruits and print the result
+        System.out.println(apples+oranges);
+    }
+}
